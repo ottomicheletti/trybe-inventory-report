804d1c7053fd846ee904f2d92e4a32af3cbeb097
ottomicheletti <michelettiotto@gmail.com>
2022-10-29 23:58:24 -0300

    perf: :zap: Changes the products variable to be a tuple instead of an array

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,20 +1,18 @@
 from inventory_report.inventory.product import Product
 
-products = [
-    (
-        1,
-        "Nicotine Polacrilex",
-        "Target Corporation",
-        "2021-02-18",
-        "2023-09-17",
-        "CR25 1551 4467 2549 4402 1",
-        "instrucao 1",
-    )
-]
+products = (
+    1,
+    "Nicotine Polacrilex",
+    "Target Corporation",
+    "2021-02-18",
+    "2023-09-17",
+    "CR25 1551 4467 2549 4402 1",
+    "instrucao 1",
+)
 
 
 def test_cria_produto():
-    product = Product(*products[0])
+    product = Product(*products)
     assert isinstance(product.id, int)
     assert isinstance(product.nome_do_produto, str)
     assert isinstance(product.nome_da_empresa, str)
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,20 +1,18 @@
 from inventory_report.inventory.product import Product
 
-products = [
-    (
-        1,
-        "Nicotine Polacrilex",
-        "Target Corporation",
-        "2021-02-18",
-        "2023-09-17",
-        "CR25 1551 4467 2549 4402 1",
-        "instrucao 1",
-    )
-]
+products = (
+    1,
+    "Nicotine Polacrilex",
+    "Target Corporation",
+    "2021-02-18",
+    "2023-09-17",
+    "CR25 1551 4467 2549 4402 1",
+    "instrucao 1",
+)
 
 
 def test_relatorio_produto():
-    product = Product(*products[0])
+    product = Product(*products)
     assert repr(product) == (
         f"O produto {product.nome_do_produto}"
         f" fabricado em {product.data_de_fabricacao}"
