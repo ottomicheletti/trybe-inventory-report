142d8531a372758500a51cc61a1e66b9b63fb602
ottomicheletti <michelettiotto@gmail.com>
2022-10-29 23:53:41 -0300

    feat(Req 02): :sparkles: Implements test_relatorio_produto() within test_product_report.py

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+products = [
+    (
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        "CR25 1551 4467 2549 4402 1",
+        "instrucao 1",
+    )
+]
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(*products[0])
+    assert repr(product) == (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
