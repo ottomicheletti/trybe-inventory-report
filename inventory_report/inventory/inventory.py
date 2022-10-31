0377f213bd682d6235d5d58d3c7665169d84b7ca
ottomicheletti <michelettiotto@gmail.com>
2022-10-31 17:30:45 -0300

    feat(Req 06): :recycle: Refactores inventory.py to have a dict with extension types. Now the reading of a file is dynamic depending solely on the extension type informed on the path

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,16 +1,28 @@
+import os
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+
+
+exts = {
+    ".csv": [
+        lambda file: csv.DictReader(file),
+        lambda inventory: list(inventory),
+    ],
+    ".json": [lambda file: json.load(file), lambda inventory: inventory],
+}
 
 
 class Inventory:
     @staticmethod
     def import_data(path: str, type: str):
+        _, file_ext = os.path.splitext(path)
         if type == "simples":
             with open(path, "r") as file:
-                inventory = csv.DictReader(file)
-                return SimpleReport.generate(list(inventory))
+                inventory = exts[file_ext][0](file)
+                return SimpleReport.generate(exts[file_ext][1](inventory))
         if type == "completo":
             with open(path, "r") as file:
-                inventory = csv.DictReader(file)
-                return CompleteReport.generate(list(inventory))
+                inventory = exts[file_ext][0](file)
+                return CompleteReport.generate(exts[file_ext][1](inventory))
