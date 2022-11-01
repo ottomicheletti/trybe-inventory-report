0fd3b5fc041ff47ffc7f7d5bba257422fd934b8a
ottomicheletti <michelettiotto@gmail.com>
2022-11-01 16:49:08 -0300

    feat(Req 11): :sparkles: Implements the main() function within main.py. Now, when passing arguments through the terminal, the program will be able to execute the right class call as the user specifies the filepath and the type of report wanted

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,23 @@
+import os
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    file_importers = {
+        ".csv": CsvImporter,
+        ".json": JsonImporter,
+        ".xml": XmlImporter,
+    }
+    _, file_ext = os.path.splitext(sys.argv[1])
+
+    if len(sys.argv) < 3:
+        sys.stderr.write("Verifique os argumentos\n")
+    else:
+        report = InventoryRefactor(file_importers[file_ext]).import_data(
+            sys.argv[1], sys.argv[2]
+        )
+        sys.stdout.write(report)
